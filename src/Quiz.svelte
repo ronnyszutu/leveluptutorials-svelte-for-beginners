fe41f9e129e7fba775ccd899796ab3b9218c44b1
szuturon <ronnyszutu@gmail.com>
2022-04-24 00:43:36 -0400

    Basic Events

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -1,17 +1,18 @@
 <script>
-    export let quizName = "Ronny Quiz";
-    // let title = "";
-    let a = 0;
-    let b = 0;
+    let result = "";
+    let correctAnswer = "b";
+    function pickAnswer(answer){
+        if (answer === correctAnswer) {
+            return (result = "Correct!");
+        }
+        result = "OOPS";
+    }
 </script>
 
 <div>
-    <h2>{quizName}</h2>
-    
-    <!-- <h4>{title}</h4>
-    <input bind:value={title} type="text" /> -->
-
-    <input type="number" bind:value={a}>
-    <input type="number" bind:value={b}>
-    <h4>{a + b}</h4>
+    <h4>{result}</h4>
+    <button on:click={() => pickAnswer('a')}>Answer A</button>
+    <button on:click={() => pickAnswer('b')}>Answer B</button>
+    <button on:click={() => pickAnswer('c')}>Answer C</button>
+    <button on:click={() => pickAnswer('d')}>Answer D</button>
 </div>
