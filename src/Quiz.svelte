34bbf149cae1088d52884619be7230da4a45471a
szuturon <ronnyszutu@gmail.com>
2022-04-25 23:47:14 -0400

    Await

--- src/Quiz.svelte
+++ src/Quiz.svelte
@@ -15,8 +15,13 @@
         const res = await fetch('https://opentdb.com/api.php?amount=10&category=21&type=multiple');
         const quiz = await res.json();
         console.log(quiz);
-        debugger;
+        return quiz;
     }
+
+    function handleClick() {
+        quiz = getQuiz();
+    }
+
 </script>
 
 <style>
@@ -26,14 +31,19 @@
 </style>
 
 <div>
-    <button on:click={getQuiz}>Get New Questions</button>
+    <button on:click={handleClick}>Get New Questions</button>
     {#if result}
         <h4>{result}</h4>
     {:else}
         <h5>Please pick an answer</h5>
     {/if}
 
-    <h3>{quiz.results[0].question}</h3>
+    {#await quiz}
+        Loading....
+    {:then data} 
+        <h3>{data.results[0].question}</h3>
+    {/await}
+
 
     {#each answers as answer}
         <button on:click={() => pickAnswer(answer)}>
